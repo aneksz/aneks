4210c7a41e100455cd3f48b78e5334530c00f548
Igor Otovic <iotovic@gmail.com>
2026-03-15 11:30:44 +1100

    config

--- dotfiles/.config/hypr/scripts/paper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-killall -9 hyprpaper 2>/dev/null; sleep 0.2; /usr/bin/hyprpaper >/dev/null 2>&1 &
--- dotfiles/.config/hypr/scripts/screenshot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DIR="$HOME/Pictures/Screenshots"
-mkdir -p "$DIR"
-TS=$(date +%Y-%m-%d_%H-%M-%S)
-OUT="$DIR/$TS.png"
-
-# pipe uncompressed ppm into satty and specify where to save the edited image
-grim -g "$(slurp -d)" -t ppm - | satty --filename - --output-filename "$OUT" --early-exit
